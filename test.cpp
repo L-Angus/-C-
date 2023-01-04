3cfd4c2c7a9d29beefb0961ea66321e1f8ece7fd
angus.li <lsd15651008321@163.com>
2023-01-04 19:08:09 +0800

    recommit test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: angus.li
+	> Mail: lsd15651008321@163.com 
+	> Created Time: 三  1/ 4 19:04:30 2023
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "hello world!" << std::endl;
+	return 0;
+}
